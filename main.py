97be217cd7505f4e4b3055d010fdfa7343e7bb7c
DukhDmytro <cowboybebop4991@gmail.com>
2019-12-10 16:01:12 +0200

    1st commit

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-import contacts
+"""module for working with user interface"""
+import sqlcontacts
 
 
-while True:
-    print("""What do you want to do?
-c - create
-r - read
-u - update
-d - delete
-q - quit
-""")
-    action = input("?").lower()
-    if action == 'q':
-        break
+def main():
+    """main function"""
+    phone_book = sqlcontacts.PhoneBook()
+    while True:
+        print("""What do you want to do?
+    c - create
+    r - read
+    u - update
+    d - delete
+    q - quit
+    """)
+        action = input("?").lower()
+        if action == 'q':
+            break
+        if action == 'c':
+            name = input("Name: ")
+            number = input("Number: ")
+            print(phone_book.create(name, number))
+        if action == "r":
+            name = input("Name: ")
+            print(phone_book.read(name))
+        if action == "u":
+            name = input("Name: ")
+            number = input("Number: ")
+            print(phone_book.update(name, number))
+        if action == "d":
+            name = input("Name: ")
+            print(phone_book.delete(name))
+
+
+if __name__ == "__main__":
+    main()
--- sqlcontacts.py
+++ sqlcontacts.py
@@ -1,12 +1,76 @@
+"""modules for working with data."""
+import sqlite3
+
+
 class PhoneBook:
+    """Class to represent phone book"""
+    def __init__(self):
+        self.data_base = sqlite3.connect(":memory:")
+        self.data_base.execute("""create table phone_book
+            (id integer primary key,
+            name varchar(30),
+            phone varchar(30)
+            )""")
+
     def create(self, name, phone):
-        pass
+        """
+        Create new contact in phone book.
+        :param name: name of contact
+        :param phone: phone number of contact
+        :return: string report to user
+        :raise ValueError in case of invalid number or name
+        """
+        if not name or not phone:
+            raise ValueError
+        self.data_base.execute("""insert into phone_book
+                            (name, phone) values (?, ?)""", (name, phone))
+        return "Added"
 
     def read(self, name):
-        pass
+        """
+        Read contact in phone book
+        :param name: name of contact
+        :return: phone of specified contact
+        :raise KeyError if contact doesnt exist
+        """
+        cursor = self.data_base.execute("""select name, phone from phone_book
+                                        where name=?""", (name,))
+        cursor = cursor.fetchall()
+        if not cursor:
+            raise KeyError
+        return int(cursor[0][1])
 
     def update(self, name, phone):
-        pass
+        """
+        Update contact in phone book
+        :param name: name to update
+        :param phone: phone number to update
+        :return: string report to user
+        :raise: KeyError if contact doesnt exist
+        """
+
+        cursor = self.data_base.execute("""select id, name from phone_book
+                                        where name=?""", (name,))
+        cursor = cursor.fetchall()
+        if not cursor:
+            raise KeyError
+        id_ = cursor[0][0]
+        self.data_base.execute("""update phone_book set phone=?
+                                        where id=?""", (phone, id_))
+        return "Updated"
 
     def delete(self, name):
-        pass
+        """
+        Delete contact from phone book
+        :param name: name of contact
+        :return: string report to user
+        :raise: KeyError if contact doesnt exist
+        """
+        cursor = self.data_base.execute("""select id, name from phone_book
+                                        where name=?""", (name,))
+        cursor = cursor.fetchall()
+        if not cursor:
+            raise KeyError
+        id_ = cursor[0][0]
+        self.data_base.execute("delete from phone_book where id=?", (id_,))
+        return "Deleted"
