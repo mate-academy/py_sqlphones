ff959ba510ba7e17ec112aea000bccfaae77917e
logotip <logotip123@yahoo.com>
2019-12-10 17:25:50 +0200

    v 1.2

--- main.py
+++ main.py
@@ -12,23 +12,28 @@ def dialog():
     u - update
     d - delete
     q - quit""")
-        action = input("?").lower()
-        if action == 'c':
-            name = input("Input new contact name \n")
-            phone = input("Input the phone \n")
-            phone_book.create(name, phone)
-        elif action == 'r':
-            name = input("Input the contact name \n")
-            print(phone_book.read(name))
-        elif action == 'u':
-            name = input("Input the contact name \n")
-            phone = input("Input new phone \n")
-            phone_book.update(name, phone)
-        elif action == 'd':
-            name = input("Input the contact name for deleting \n")
-            phone_book.delete(name)
-        elif action == 'q':
-            break
+        try:
+            action = input("?").lower()
+            if action == 'c':
+                name = input("Input new contact name \n")
+                phone = input("Input the phone \n")
+                phone_book.create(name, phone)
+            elif action == 'r':
+                name = input("Input the contact name \n")
+                print(phone_book.read(name))
+            elif action == 'u':
+                name = input("Input the contact name \n")
+                phone = input("Input new phone \n")
+                phone_book.update(name, phone)
+            elif action == 'd':
+                name = input("Input the contact name for deleting \n")
+                phone_book.delete(name)
+            elif action == 'q':
+                break
+            else:
+                print("incorrect input")
+        except KeyError:
+            print("incorrect input")
 
 
 if __name__ == "__main__":
